1be25086862bf320882cebbe4840e771da8065de
AdityaChaudhry23 <adityachaudhry200323@gmail.com>
2025-05-09 12:44:02 +0100

    Completed UART Section of the couse built a UART Reciever and Transmitter

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/84/60ca5ba8c62c00101f3cd16a290dd04c
@@ -0,0 +1,14 @@
+/*
+ * uart.h
+ *
+ *  Created on: May 9, 2025
+ *      Author: aditya-chaudhry
+ */
+
+#ifndef UART_H_
+#define UART_H_
+#include <stdint.h>
+#include "stm32f4xx.h"
+void uar2_tx_init(void);
+
+#endif /* UART_H_ */
---
+++ 7_uart_modular/Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f410rx.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/core_cm4.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_version.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/uart.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f410rx.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/core_cm4.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_version.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Include/mpu_armv7.h:
+/home/aditya-chaudhry/Desktop/STM32\ Development/Beginner\ Bare-Metal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
